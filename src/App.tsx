60a42692483efd06c4220d99cf40f1e025ee5845
Marcelo Kortmann <kortmma@DEAGM03905.sickcn.net>
2023-08-10 18:04:41 +0200

    add: interpolation and pluralization

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { useTranslation, Trans } from 'react-i18next';
@@ -10,18 +10,23 @@ const lngs: any = {
 };
 
 function App() {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const [count, setCounter] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>
           {Object.keys(lngs).map((lng) => (
-            <button key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={() => i18n.changeLanguage(lng)}>
+            <button key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={() => {i18n.changeLanguage(lng); setCounter(count + 1)}}>
               {lngs[lng].nativeName}
             </button>
           ))}
         </div>
+        <p>
+          <i>{t('counter', { count })}</i>
+        </p>
         <p>
         <Trans i18nKey="description.part1">
             Edit <code>src/App.js</code> and save to reload.
